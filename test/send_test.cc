a4cb5e13878b2d93cf138cd86bb61c6921457c1a
Pablo Stebler <pablo@stebler.xyz>
2022-06-09 00:48:27 +0200

    Add send_test

---
+++ test/send_test.cc
@@ -0,0 +1,196 @@
+/* Copyright (c) 2020, Stanford University
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <iostream>
+#include <vector>
+
+#include <Homa/Homa.h>
+#include <Homa/Drivers/DPDK/DpdkDriver.h>
+#include <Cycles.h>
+#include <TimeTrace.h>
+#include <docopt.h>
+
+#include "Output.h"
+
+static const char USAGE[] = R"(DPDK Driver Test.
+
+    Usage:
+        dpdk_test [options] <port> <size> <frequency> (--server | <server_address>)
+
+    Options:
+        -h --help           Show this screen.
+        --version           Show version.
+)";
+
+int
+main(int argc, char* argv[])
+{
+    std::map<std::string, docopt::value> args =
+            docopt::docopt(USAGE, {argv + 1, argv + argc},
+                    true,                       // show help if requested
+                    "DPDK Driver Test");        // version string
+
+    int size = args["<size>"].asLong();
+    int frequency = args["<frequency>"].asLong();
+    int port = args["<port>"].asLong();
+    bool isServer = args["--server"].asBool();
+    std::string server_address_string;
+    if (!isServer) {
+        server_address_string = args["<server_address>"].asString();
+    }
+
+    Homa::Drivers::DPDK::DpdkDriver driver(port);
+    std::unique_ptr<Homa::Transport> transport(Homa::Transport::create(&driver, 0));
+    std::vector<uint8_t> data(size);
+    std::fill(data.begin(), data.end(), 0);
+
+    if (isServer) {
+        std::cout << "Server address: " << driver.addressToString(driver.getLocalAddress())
+                 << " size: " << size
+                 << " frequency: " << frequency
+                 << " port: " << port
+                 << std::endl;
+
+        uint64_t duration = 10;
+        uint64_t count = frequency * duration;
+        uint64_t total_start;
+
+        while (true) {
+            Homa::unique_ptr<Homa::InMessage> in = transport->receive();
+            if (in != nullptr) {
+                in->acknowledge();
+                uint64_t id;
+                in->get(0, &id, sizeof(id));
+                if (id % 100 == 0) { std::cout << id << std::endl; };
+                if (id == 0) {
+                    total_start = PerfUtils::Cycles::rdtsc();
+                } else if (id == count - 1) {
+                    uint64_t total_stop = PerfUtils::Cycles::rdtsc();
+                    double total_time = PerfUtils::Cycles::toSeconds(total_stop - total_start);
+                    uint64_t throughput = (count * size) / total_time * 8;
+                    std::cout << "Throughput: " << throughput << " b/s" << std::endl;
+                }
+            }
+
+            transport->poll();
+        }
+    } else {
+        std::cout << "Client address: " << driver.addressToString(driver.getLocalAddress())
+                 << " size: " << size
+                 << " frequency: " << frequency
+                 << " port: " << port
+                 << std::endl;
+        Homa::Driver::Address server_address =
+           driver.getAddress(&server_address_string);
+
+        while (true) {
+            uint64_t out_id = UINT64_MAX;
+            Homa::unique_ptr<Homa::OutMessage> out = transport->alloc();
+            out->append(&out_id, sizeof(out_id));
+            out->send(server_address);
+
+            while (true) {
+                transport->poll();
+                PerfUtils::Cycles::sleep(1000 * 1000 /* 1 second */);
+
+                switch (out->getStatus()) {
+                default:
+                    continue;
+                case Homa::OutMessage::Status::COMPLETED:
+                    break;
+                case Homa::OutMessage::Status::FAILED:
+                    out = transport->alloc();
+                    out->append(&out_id, sizeof(out_id));
+                    out->send(server_address);
+                    continue;
+                }
+                break;
+            }
+
+            if (out->getStatus() == Homa::OutMessage::Status::COMPLETED)
+                break;
+        }
+
+        std::cout << "Successfully connected to the server" << std::endl;
+
+        uint64_t duration = 10;
+        uint64_t count = frequency * duration;
+        uint64_t period = PerfUtils::Cycles::fromSeconds(duration) / count;
+        uint64_t total_start = PerfUtils::Cycles::rdtsc();
+        uint64_t total_delay = 0;
+        driver.cork();
+
+        //uint64_t done_count = 0;
+        //std::vector<Homa::unique_ptr<Homa::OutMessage>> out_vector(count);
+
+        uint64_t out_id = 0;
+        while (out_id < count) {
+        //while (done_count < count) {
+            //if (out_id < count && PerfUtils::Cycles::rdtsc() - total_start >= out_id * period) {
+            int64_t delay = out_id * period - (PerfUtils::Cycles::rdtsc() - total_start);
+            if (delay <= 0) {
+                Homa::unique_ptr<Homa::OutMessage> out = transport->alloc();
+                out->append(&out_id, sizeof(out_id));
+                out->append(data.data(), data.size());
+                out->send(server_address);
+                out_id++;
+                if (out_id % 100 == 0) { std::cout << out_id << std::endl; }
+                //out_vector[out_id++] = std::move(out);
+                //std::cout << out_id << std::endl;
+                do {
+                    transport->poll();
+                    driver.uncork();
+                    driver.cork();
+                } while (out->getStatus() != Homa::OutMessage::Status::COMPLETED);
+            } else {
+                total_delay += delay;
+                PerfUtils::Cycles::sleep(PerfUtils::Cycles::toMicroseconds(delay));
+            }
+
+            //transport->poll();
+
+            /*for (; done_count < out_id; done_count++) {
+                switch (out_vector[done_count]->getStatus()) {
+                default:
+                    break;
+                case Homa::OutMessage::Status::COMPLETED:
+                case Homa::OutMessage::Status::SENT:
+                    out_vector[done_count] = nullptr;
+                    if (done_count % 10 == 0) { std::cout << "ok " << done_count << std::endl; }
+                    continue;
+                case Homa::OutMessage::Status::FAILED:
+                    std::cout << "fail " << done_count << std::endl;
+                    //Homa::unique_ptr<Homa::OutMessage> out = transport->alloc();
+                    //out->append(&out_id, sizeof(out_id));
+                    //out->append(data.data(), data.size());
+                    //out->send(server_address);
+                    //out_vector[done_count] = std::move(out);
+                    break;
+                }
+                break;
+            }*/
+        }
+
+        uint64_t total_stop = PerfUtils::Cycles::rdtsc();
+        double total_time = PerfUtils::Cycles::toSeconds(total_stop - total_start);
+        uint64_t throughput = (count * size) / total_time * 8;
+        double load = 1.0 - (double)total_delay / (total_stop - total_start);
+
+        std::cout << "Throughput: " << throughput << " b/s" << std::endl;
+        std::cout << "Load: " << load << std::endl;
+    }
+
+    return 0;
+}
